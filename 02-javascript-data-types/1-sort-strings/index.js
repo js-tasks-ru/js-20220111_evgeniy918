287ec63156b42fc189fbe8407cad5d24f5ac7f67
Евгений Пеньков <epenkov@adsterra.com>
2022-01-30 23:14:20 +0300

    add sort function

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -5,5 +5,24 @@
  * @returns {string[]}
  */
 export function sortStrings(arr, param = 'asc') {
-
+  let mapped = arr.map(function(el, i) {
+    return { index: i, value: el};
+  });
+  if (param === 'asc') {
+    mapped.sort(function (a, b) {
+      return a.value.localeCompare(b.value, 'kf', {caseFirst: 'upper'});
+    });
+    arr = mapped.map(function(el) {
+      return arr[el.index];
+    });
+  }
+  if (param === 'desc') {
+    mapped.sort(function (b, a) {
+      return a.value.localeCompare(b.value, 'ru', {caseFirst: 'upper'});
+    });
+    arr = mapped.map(function(el) {
+      return arr[el.index];
+    });
+  }
+  return arr;
 }
