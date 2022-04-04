52b642dc9dc0222bbbddb23f9a9ab62df010d4c7
Евгений Пеньков <epenkov@adsterra.com>
2022-04-04 10:22:01 +0300

    add 3 lesson

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -4,6 +4,24 @@
  * @param {number} size - the allowed size of consecutive identical symbols
  * @returns {string} - the new string without extra symbols according passed size
  */
-export function trimSymbols(string, size) {
+export const trimSymbols = (string, size) => {
+  if (size === 0) { return ''; }
+  if (size === undefined) { return string; }
 
-}
+  let count = 0;
+  const result = [];
+
+  for (const char of string) {
+    if (result[result.length - 1] === char) {
+      if (count < size) {
+        result.push(char);
+        count += 1;
+      }
+    } else {
+      count = 1;
+      result.push(char);
+    }
+  }
+
+  return result.join('');
+};
